541f2153c4fbc1b63de90c940dd869acdab89de2
Alex <cbm.mailbox@gmail.com>
2010-01-12 18:14:44 +0200

    Added Mutexes to generic linux platform

---
+++ KArch/src/hal/generic_linux/mutex.h
@@ -0,0 +1,23 @@
+#ifndef _mutex_h_
+#define _mutex_h_ 1
+
+#include <pthread.h>
+
+class Mutex {
+
+    public:
+
+        Mutex();
+
+        virtual ~Mutex();
+
+        virtual void lock() const;
+
+        virtual void release() const;
+
+    private:
+
+        mutable pthread_mutex_t mtex;
+}
+
+#endif // _mutex_h_
