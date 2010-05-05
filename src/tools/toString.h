e1e442300a4d41756049b38834b574db5bee0a01
Alex <cbm.mailbox@gmail.com>
2010-05-05 12:18:39 +0300

    Added toString function

---
+++ src/tools/toString.h
@@ -0,0 +1,15 @@
+#ifndef _toString_h_ 
+#define _toString_h_  1
+
+#include <sstream>
+#include <string>
+
+template<class T>
+std::string _toString( T val ) {
+    std::ostringstream ost;
+    ost << val;
+    ost.flush();
+    return ost.str();
+}
+
+#endif // _toString_h_
