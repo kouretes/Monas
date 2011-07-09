8da9cf760a93937bcc84c228ccfc9d4c81d5f8ac
angeltop <antopalidou@hotmail.com>
2011-07-09 17:16:13 +0300

    Orientation added in Kick activity NO BACK KICK!!!!!!!!!!!

--- src/activities/Kick/Kick.h
+++ src/activities/Kick/Kick.h
@@ -6,6 +6,8 @@
 #include "messages/AllMessagesHeader.h"
 #include "tools/logger.h"
 #include "tools/toString.h"
+#include "../BehaviorConst.h"
+#include "../Localization/KLocalization.h"
 
 class Kick : public IActivity {
 			
@@ -17,10 +19,19 @@ public:
 	
 	std::string GetName ();
 private:
+	KLocalization k;
+	int orientation;
+	float by;
+	float blueGoalX, blueGoalY, yellowGoalX, yellowGoalY, oppGoalX, oppGoalY;
 	MotionActionMessage* amot;	
 	boost::shared_ptr<const GameStateMessage> gsm;		
 	boost::shared_ptr<const WorldInfo> wimsg;
+	boost::shared_ptr<const KickOffMessage> kcm;
+	boost::shared_ptr<const PlayerNumberMessage> pnm;
 	ReturnToPositionMessage* rpm;
+	bool readConf;
+	
+	bool readGoalConfiguration(const std::string& file_name);
 };
 
 #endif // _Kick_h_
