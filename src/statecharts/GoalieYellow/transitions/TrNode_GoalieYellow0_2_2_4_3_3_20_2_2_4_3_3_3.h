0e459c6b228aa428201bcb2b5a22d5c640530aa6
angeltop <antopalidou@hotmail.com>
2012-03-29 15:01:46 +0300

    Goalie fix again!

--- src/statecharts/GoalieYellow/transitions/TrNode_GoalieYellow0_2_2_4_3_3_20_2_2_4_3_3_3.h
+++ src/statecharts/GoalieYellow/transitions/TrNode_GoalieYellow0_2_2_4_3_3_20_2_2_4_3_3_3.h
@@ -11,12 +11,12 @@ public:
 
 	}
 	bool Eval() {
-		/* worldstate.State.GameStateMessage== NULL || worldstate.State.GameStateMessage.player_state()==PLAYER_READY || !robotInPosition(worldstate.Data.WorldInfo, behavior.State.PositionMessage) */
+		/* worldstate.State.GameStateMessage== NULL || worldstate.State.GameStateMessage.player_state()!=PLAYER_READY || robotInPosition(worldstate.Data.WorldInfo, behavior.State.PositionMessage) */
 		boost::shared_ptr<const GameStateMessage> var_1263977940 = _blk->readState<GameStateMessage> ("worldstate" );
 		boost::shared_ptr<const WorldInfo> var_1111221333 = _blk->readData<WorldInfo> ("worldstate" );
 		boost::shared_ptr<const PositionMessage> var_1868674971 = _blk->readState<PositionMessage> ("behavior" );
 	
-				return ( var_1263977940.get()==0 ||  (var_1263977940.get()!=0 && var_1263977940->player_state()==PLAYER_READY) || !robotInPosition(var_1111221333, var_1868674971) );	
+				return ( var_1263977940.get()==0 ||  (var_1263977940.get()!=0 && var_1263977940->player_state()!=PLAYER_READY) || robotInPosition(var_1111221333, var_1868674971) );	
     }
 };
 		
