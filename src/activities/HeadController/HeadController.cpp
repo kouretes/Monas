17df8a6ce7046e308d8e328ef768683f833b045c
eldr4d <nikofinas@gmail.com>
2012-10-28 19:49:19 +0200

    Mathcommon: make distance a function

--- src/activities/HeadController/HeadController.cpp
+++ src/activities/HeadController/HeadController.cpp
@@ -698,9 +698,3 @@ bool HeadController::readGoalConfiguration(const std::string& file_name)
 	return true;
 }
 
-float HeadController::dist(float x1, float y1, float x2, float y2)
-{
-	return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-}
-
-
