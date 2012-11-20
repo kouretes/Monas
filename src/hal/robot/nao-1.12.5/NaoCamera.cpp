1ffeb8b01f32ae5cf0b7a219e19bb845aeaa812f
eldrad <nikofinas@gmail.com>
2012-11-20 03:25:11 +0200

    Fallback from AUTCUP
    Signed-off-by: eldrad <nikofinas@gmail.com>

--- src/hal/robot/nao-1.12.5/NaoCamera.cpp
+++ src/hal/robot/nao-1.12.5/NaoCamera.cpp
@@ -386,7 +386,7 @@ void NaoCamera::initDefaultControlSettings()
   setControlSetting(V4L2_CID_AUTO_WHITE_BALANCE, 0);
   setControlSetting(V4L2_CID_AUTOGAIN, 0);
   setControlSetting(V4L2_CID_EXPOSURE_AUTO, V4L2_EXPOSURE_MANUAL);
-  setControlSetting(V4L2_CID_GAIN,0xB0);
+  setControlSetting(V4L2_CID_GAIN,0xF0);
   setControlSetting(V4L2_CID_BACKLIGHT_COMPENSATION, 0);
   setControlSetting(V4L2_CID_SAT_AUTO, 0);
   setControlSetting(V4L2_CID_HUE_AUTO, 0);
@@ -399,7 +399,7 @@ void NaoCamera::initDefaultControlSettings()
   setControlSetting(V4L2_CID_CONTRAST, 96);
 
   setControlSetting(V4L2_CID_AWB_G_CHANNEL_GAIN,64);
-  setControlSetting(V4L2_CID_RED_BALANCE,96 ); //128
+  setControlSetting(V4L2_CID_RED_BALANCE,128 ); //128
   setControlSetting(V4L2_CID_BLUE_BALANCE,128);
 
   usleep(300*1000);
