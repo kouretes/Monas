64da9b1083c59305c57264ed01d7b9efbcee5ea2
eldrad <nikofinas@gmail.com>
2013-02-26 00:22:58 +0200

    New colors for the eyes of the Vision:P
    Signed-off-by: eldrad <nikofinas@gmail.com>

--- src/activities/Vision/Vision.cpp
+++ src/activities/Vision/Vision.cpp
@@ -350,27 +350,25 @@ void Vision::fetchAndProcess()
 
 	if (obs.regular_objects_size() > 0)
 	{
-		bool yellow = false, blue = false;
+		bool yellow = false;
 		::google::protobuf::RepeatedPtrField<const ::NamedObject>::const_iterator ptr = obs.regular_objects().begin();
 
 		while (ptr != obs.regular_objects().end())
 		{
 			if ((*ptr).object_name().c_str()[0] == 'Y')
 				yellow = true;
-			else if ((*ptr).object_name().c_str()[0] == 'S')
-				blue = true;
-
 			++ptr;
 		}
 
 		LedValues* l = leds.add_leds();
 		l->set_chain("r_eye");
 
-		if (obs.regular_objects_size() == 1)
+		if (obs.regular_objects_size() == 1 && obs.regular_objects(0).object_name().compare("Yellow")==0)
 		{
+			l->set_color("blue");
+		}else if(obs.regular_objects_size() == 1){
 			l->set_color("yellow");
-		}
-		else if (obs.regular_objects_size() == 2)
+		}else if (obs.regular_objects_size() == 2)
 		{
 			l->set_color("purple");
 		}
@@ -425,7 +423,7 @@ void Vision::publishObstacles(std::vector<KVecInt2> points) const
 		 */
 	}
 
-	_blk.publishSignal(result, "obstacle");
+	_blk.publishSignal(result, "pathplanning");
 }
 
 KVecFloat2 Vision::simpleRotation(KVecFloat2 const& i) const
