15a303f953592b54e0d8b55d5f4c6e03f0d8a964
vosk <vosk@vosk-N411Z.(none)>
2013-06-18 01:29:52 +0300

    Shutdown
    Fixed multicast shutdown
    local build now honors SIGINT and shutsdown

--- src/hal/target_linux/main.cpp
+++ src/hal/target_linux/main.cpp
@@ -1,36 +1,38 @@
 #include <iostream>
 
 #include <string>
+#include <signal.h>
 
 #include "core/architecture/talws.h"
 #include "core/architecture/configurator/Configurator.hpp"
 
+Talws *tal;
+volatile bool  running;
+void
+termination_handler (int signum)
+{
+	std::cout<<"Signaled, terminating"<<std::endl;
+	running=false;
 
-//#include "../../hal/generic_linux/pthread.h"
-
-//namespace {  //FIXME
-//    Thread* CreatePThread(Threadable * obj, ThreadConfig cfg) {
-//        return new PThread(obj,cfg);
-//    }
-//    const bool registered_pthread_temp = ThreadFactory::Instance()->Register("PThread", CreatePThread)  ;
-//}
-
+}
 
 int main ()
 {
-	std::cout << "Main: Inst" << std::endl;
+	running=false;
 	Configurator::Instance().initConfigurator("config","","");
-	Talws tal;
+	std::cout << "Main: Constructing Talws" << std::endl;
+	tal= new Talws();
 	std::cout << "Main: Starting" << std::endl;
-	tal.Start();
-	std::cout << "Main: Sleeping" << std::endl;
+	tal->Start();
+	std::cout << "Main: Done!" << std::endl;
 
-	while(1)
-		usleep(100000000);
+	running=true;
+	signal (SIGINT, termination_handler);
+	while(running)
+		usleep(1000);
 
 	std::cout << "Main: Stoping" << std::endl;
-	tal.Stop();
-	usleep(3000000);
+	tal->Stop();
 	std::cout << "Main: Exiting" << std::endl;
 	return 0;
 }
