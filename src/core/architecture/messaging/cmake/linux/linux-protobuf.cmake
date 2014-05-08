08f7f111814fd07bf501aa7dc8af772170eaa8e5
vosk <vosk@vosk-N411Z.(none)>
2014-05-08 15:57:47 +0300

    CMake refactor
    -remove aibo stubs..
    -refactor protobuf library hooks, if localy build is not available use system
    -use TargetRobot=host to build KMonitor
    -remove narukom debris

--- src/core/architecture/messaging/cmake/linux/linux-protobuf.cmake
+++ src/core/architecture/messaging/cmake/linux/linux-protobuf.cmake
@@ -1,34 +1,10 @@
-#find_package(PkgConfig)
-#pkg_check_modules(LIBProtobuf QUIET protobuf)
-#set(LIBProtobuf_DEFINITIONS ${LIBProtobuf_CFLAGS_OTHER})
 
-#message ( STATUS "Searching for protobuf libraries")
-
-
-#find_library(LIBProtobuf_LIBRARY protobuf
-#    HINTS ${LIBProtobuf_LIBDIR} ${LIBProtobuf_LIBRARY_DIRS} )
-
-#set(LIBProtobuf_LIBRARIES ${LIBProtobuf_LIBRARY} )
-#set(LIBProtobuf_INCLUDE_DIRS ${LIBProtobuf_INCLUDE_DIR} )
-
-
-#include(FindPackageHandleStandardArgs)
-
-# handle the QUIETLY and REQUIRED arguments and set LIBXML2_FOUND to TRUE
-# if all listed variables are TRUE
-#find_package_handle_standard_args(LIBProtobuf  DEFAULT_MSG
- #   LIBProtobuf_LIBRARY LIBProtobuf_INCLUDE_DIR)
-
-#mark_as_advanced(LIBProtobuf_INCLUDE_DIR LIBProtobuf_LIBRARY )
-
-
-#if( "${LIBProtobuf_LIBRARY}" STREQUAL "LIBProtobuf_LIBRARY-NOTFOUND")
-#    set( ProtobufLibrary ${ConfigDir}/${TargetPlatform}/lib/libprotobuf.a)
-#    set(  LIBProtobuf_LIBRARY ${ProtobufLibrary} )
-#else( "${LIBProtobuf_LIBRARY}" STREQUAL "LIBProtobuf_LIBRARY-NOTFOUND")
-#    set( ProtobufLibrary ${LIBProtobuf_LIBRARY})
-#endif( "${LIBProtobuf_LIBRARY}" STREQUAL "LIBProtobuf_LIBRARY-NOTFOUND")
-
-
-set( ProtobufLibrary ${ConfigDir}/${TargetPlatform}/lib/libprotobuf.a)
+set(ProtobufPrebuild  ${ConfigDir}/${TargetPlatform}/lib/${CMAKE_SYSTEM_PROCESSOR}/libprotobuf.a)
+if(EXISTS ProtobufPrebuild)
+	set( ProtobufLibrary ProtobufPrebuild)
+else()
+	find_package(Protobuf)
+	message(STATUS ${PROTOBUF_LIBRARY})
+	set( ProtobufLibrary ${PROTOBUF_LIBRARY})
+endif()
 message ( STATUS "Protobuf lib path: ${ProtobufLibrary}")
